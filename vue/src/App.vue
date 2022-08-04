9a51d81f6edcfad82bbfefb8dadd66d59733ce13
webapp99 <codewithpranta@gmail.com>
2022-08-04 22:59:32 +0600

    Testing v2

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,22 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+import { mapState } from 'vuex';
+
+export default{
+  components: {
+    HelloWorld
+  },
+  computed: {
+    ...mapState(['user'])
+  }
+}
 </script>
 
 <template>
+  <pre>{{user}}</pre>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
