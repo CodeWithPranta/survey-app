82ff598d04cf856f029ac78adaa08c298ea67614
webapp99 <codewithpranta@gmail.com>
2022-08-05 11:27:12 +0600

    load-shedding

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,9 +1,9 @@
 <template>
- <h1>Dashboard</h1>
+ <h1>Login</h1>
 </template>
 
 <script>
 export default {
-  name: "Dashboard"
+  name: "Login"
 }
 </script>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,9 +1,9 @@
 <template>
- <h1>Dashboard</h1>
+ <h1>Register</h1>
 </template>
 
 <script>
 export default {
-  name: "Dashboard"
+  name: "Register"
 }
 </script>
